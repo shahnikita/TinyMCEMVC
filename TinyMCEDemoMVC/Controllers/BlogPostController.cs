333e8247b5bf1074bfc93fd00a87d5270ee5c128
unknown <nikitashah@RANGAMBRD05.rangamindia.com>
2015-05-01 07:03:38 -0400

    New proj

---
+++ TinyMCEDemoMVC/Controllers/BlogPostController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TinyMCEDemoMVC.Models;
+
+namespace TinyMCEDemoMVC.Controllers
+{
+    public class BlogPostController : Controller
+    {
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        public ActionResult Create_post(BlogPost model)
+        {
+
+            ViewBag.HtmlContent = model.Content;
+
+            return View(model);
+
+        }
+    }
+}
---
+++ TinyMCEDemoMVC/Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinyMCEDemoMVC.Models
+{
+    public class BlogPost
+    {
+        public string Title { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Tags { get; set; }
+          [UIHint("tinymce_jquery_full"), AllowHtml]
+        public string Content { get; set; }
+    }
+}
